aaafe4d825ef4a844bef9220d7d55fc8adab1fc4
mikhailyatsenko <misha.ggg@gmail.com>
2024-11-04 20:30:01 +0100

    wip

--- src/entities/ToggleCharacteristicButton/ui/ToggleCharacteristicButton.tsx
+++ src/entities/ToggleCharacteristicButton/ui/ToggleCharacteristicButton.tsx
@@ -1,8 +1,16 @@
-import MyIcon from '../../../shared/assets/pet-friendly-icon.svg?react';
+import OutdoorIcon from '../../../shared/assets/chair-icon.svg?react';
+import EatsIcon from '../../../shared/assets/eats-icon.svg?react';
+import FilterCoffeIcon from '../../../shared/assets/filter-coffee-icon.svg?react';
+import AtmosphereIcon from '../../../shared/assets/lamp-icon.svg?react';
+import PetsIcon from '../../../shared/assets/pets-icon.svg?react';
+import PriceIcon from '../../../shared/assets/price-icon.svg?react';
+import StaffIcon from '../../../shared/assets/staff-icon.svg?react';
+import WifiIcon from '../../../shared/assets/wifi-icon.svg?react';
 import cls from './ToggleCharacteristicButton.module.scss';
 
 interface CustomButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   pressed: boolean;
+  number: number;
   onClick: () => void;
   name: string;
 }
@@ -16,7 +24,7 @@ export const ToggleCharacteristicButton: React.FC<CustomButtonProps> = ({
   return (
     <button onClick={onClick} className={`${cls.buttonChar} ${pressed ? cls.pressed : ''}`} {...props}>
       <div className={cls.charIcon}>
-        <MyIcon />
+        <OutdoorIcon width={'16'} height={'16'} />
       </div>
       {children}
     </button>
