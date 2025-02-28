3f9635973ef69ae2f9cda8bfe7baa4cba1bcf227
mikhailyatsenko <misha.ggg@gmail.com>
2025-02-28 10:13:29 +0100

    fix modal component propsInteface name

--- src/shared/ui/Modal/ui/Modal.tsx
+++ src/shared/ui/Modal/ui/Modal.tsx
@@ -1,13 +1,13 @@
 import { type ReactNode, useEffect } from 'react';
 import cls from './Modal.module.scss';
 
-interface LoginPopupProps {
+interface ModalProps {
   children: ReactNode;
   onClose: () => void;
   widthOnDesktop?: 400 | 600 | 800;
 }
 
-export const Modal = ({ children, onClose, widthOnDesktop = 400 }: LoginPopupProps) => {
+export const Modal = ({ children, onClose, widthOnDesktop = 400 }: ModalProps) => {
   useEffect(() => {
     const handleEscKey = (event: KeyboardEvent) => {
       if (event.key === 'Escape') {
