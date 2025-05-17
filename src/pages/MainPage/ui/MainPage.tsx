d0c35df8fe095d5de575e267e66f41cad371846b
mikhailyatsenko <misha.ggg@gmail.com>
2025-05-17 15:28:02 +0200

    refactor: move initial place load in main page, refactor components to use new places store

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,12 +1,49 @@
+import { useEffect, useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
 import { MainMap } from 'widgets/Map';
 import { PlacesList } from 'widgets/PlacesList';
+import { ShowFavoritePlaces } from 'features/ShowFavoritePlaces';
+import { useGetAllPlacesQuery } from 'shared/generated/graphql';
+import { usePlacesStore, setPlaces } from 'shared/stores/places';
+import { Loader } from 'shared/ui/Loader';
 
 export const MainPage = () => {
+  const filteredPlaces = usePlacesStore((state) => state.filteredPlaces);
+  const showFavorites = usePlacesStore((state) => state.showFavorites);
+  const places = usePlacesStore((state) => state.places);
+
+  const { data, loading } = useGetAllPlacesQuery();
+
+  useEffect(() => {
+    if (data) {
+      setPlaces(data.places);
+    }
+  }, [data]);
+
+  const favoritePlaces = useMemo(() => {
+    return places.filter((place) => place.properties.isFavorite);
+  }, [places]);
+
+  const placesToDisplay = useMemo(() => {
+    if (showFavorites) {
+      return favoritePlaces;
+    }
+
+    return filteredPlaces?.length ? filteredPlaces : places;
+  }, [showFavorites, places, filteredPlaces, favoritePlaces]);
+
+  // This is a feature collection of places to properly display on the map using mapbox
+  const placesGeo = {
+    type: 'FeatureCollection' as const,
+    features: placesToDisplay ?? [],
+  };
+
   return (
     <>
-      <MainMap />
-      <PlacesList />
+      {loading ? <Loader /> : null}
+      <MainMap placesGeo={placesGeo} />
+      <PlacesList places={placesToDisplay} />
+      <ShowFavoritePlaces showFavorites={showFavorites} favoritesQuantity={favoritePlaces.length} />
       <Outlet />
     </>
   );
--- src/widgets/Map/ui/MainMap.tsx
+++ src/widgets/Map/ui/MainMap.tsx
@@ -1,51 +1,11 @@
 import 'mapbox-gl/dist/mapbox-gl.css';
-import { useEffect, useMemo } from 'react';
+import { type MainMapProps } from 'widgets/Map/types';
 import { LoadMap } from 'features/LoadMap';
-import { type PlacesDataWithGeo } from 'features/LoadMap/types';
 
-import { ShowFavoritePlaces } from 'features/ShowFavoritePlaces';
-import { useGetAllPlacesQuery } from 'shared/generated/graphql';
-import { setPlaces, usePlacesStore } from 'shared/stores/places';
-import { Loader } from 'shared/ui/Loader';
-
-export const MainMap = () => {
-  const filteredPlaces = usePlacesStore((state) => state.filteredPlaces);
-  const showFavorites = usePlacesStore((state) => state.showFavorites);
-  const places = usePlacesStore((state) => state.places);
-
-  const { data, loading } = useGetAllPlacesQuery();
-
-  useEffect(() => {
-    if (data) {
-      setPlaces(data.places);
-    }
-  }, [data]);
-
-  const favoritePlaces = useMemo(() => {
-    return places.filter((place) => place.properties.isFavorite);
-  }, [places]);
-
-  const placesToDisplay = useMemo(() => {
-    if (showFavorites) {
-      return favoritePlaces;
-    }
-
-    return filteredPlaces?.length ? filteredPlaces : places;
-  }, [showFavorites, places, filteredPlaces, favoritePlaces]);
-
-  // This is a feature collection of places to properly display on the map using mapbox
-  const placesGeo = {
-    type: 'FeatureCollection' as const,
-    features: placesToDisplay ?? [],
-  };
-
+export const MainMap = ({ placesGeo }: MainMapProps) => {
   return (
-    <>
-      {loading ? <Loader /> : null}
-      <div style={{ width: '100dvw', height: 'calc(100dvh - 60px)', zIndex: 1 }}>
-        {placesGeo && <LoadMap placesGeo={placesGeo as PlacesDataWithGeo} />}
-      </div>
-      <ShowFavoritePlaces showFavorites={showFavorites} favoritesQuantity={favoritePlaces.length} />
-    </>
+    <div style={{ width: '100dvw', height: 'calc(100dvh - 60px)', zIndex: 1 }}>
+      <LoadMap placesGeo={placesGeo} />
+    </div>
   );
 };
