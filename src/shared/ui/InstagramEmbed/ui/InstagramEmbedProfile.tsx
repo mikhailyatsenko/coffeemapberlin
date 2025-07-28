5601be30674b3cd16393a8677b91b8a822d4cf2a
mikhailyatsenko <misha.ggg@gmail.com>
2025-07-28 13:49:55 +0200

    feat: fix background detailed card

--- src/shared/ui/InstagramEmbed/ui/InstagramEmbedProfile.tsx
+++ src/shared/ui/InstagramEmbed/ui/InstagramEmbedProfile.tsx
@@ -4,13 +4,19 @@ import cls from './InstagramEmbed.module.scss';
 
 interface InstagramEmbedProfileProps {
   instaLink: string;
+  image: string;
   normalView: boolean;
 }
-export const InstagramEmbedProfile = ({ instaLink, normalView = false }: InstagramEmbedProfileProps) => {
+export const InstagramEmbedProfile = ({ instaLink, image, normalView = false }: InstagramEmbedProfileProps) => {
   return (
     <div className={`${cls.embedWrapper} ${normalView ? cls.backDrop : ''}`}>
       <div className={`${cls.embed}  ${normalView ? cls.normalView : ''}`}>
-        <InstagramEmbed embedPlaceholder={<BackgroundTexture />} url={instaLink} width={'100%'} igVersion="v2" />
+        <InstagramEmbed
+          embedPlaceholder={<BackgroundTexture image={image} />}
+          url={instaLink}
+          width={'100%'}
+          igVersion="v2"
+        />
       </div>
     </div>
   );
