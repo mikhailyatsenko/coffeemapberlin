373c4633d6e71531060cd6e5d6f89532a5790345
mikhailyatsenko <misha.ggg@gmail.com>
2025-08-06 15:21:11 +0200

    feat: add openning hours initial setup

---
+++ src/entities/OpeningHours/index.ts
@@ -0,0 +1,2 @@
+export { OpeningHours } from './ui';
+export type { OpeningHoursProps, OpeningHoursItem } from './types';
---
+++ src/entities/OpeningHours/types/index.ts
@@ -0,0 +1,9 @@
+export interface OpeningHoursItem {
+  day: string;
+  hours: string;
+}
+
+export interface OpeningHoursProps {
+  openingHours: OpeningHoursItem[];
+  className?: string;
+}
