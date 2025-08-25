91f3f3b1e33538a307fbe1a2258b461edbd9d72f
mikhailyatsenko <misha.ggg@gmail.com>
2025-08-25 22:26:09 +0200

    feat: add purgeCss

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import { defineConfig } from 'vite';
+import type { PluginOption } from 'vite';
+import purgeCss from 'vite-plugin-purgecss';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), svgr()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    svgr(),
+    purgeCss({
+      content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+      safelist: [/^maplibregl-/],
+    }) as PluginOption,
+  ],
   define: {
     'process.env': { ...process.env, VITE_ENV: process.env.VITE_ENV ?? 'development' },
   },
