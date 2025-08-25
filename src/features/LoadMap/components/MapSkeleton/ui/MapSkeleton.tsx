b0364881691a8cce6f11fb0852524914a113f02d
mikhailyatsenko <misha.ggg@gmail.com>
2025-08-25 15:45:27 +0200

    hotfix: skeleton

--- src/features/LoadMap/components/MapSkeleton/ui/MapSkeleton.tsx
+++ src/features/LoadMap/components/MapSkeleton/ui/MapSkeleton.tsx
@@ -6,40 +6,10 @@ const Map = () => (
   </div>
 );
 
-// const PlaceCardSkeleton = () => (
-//   <div className={cls.placeCardSkeleton}>
-//     <div className={cls.cardContent}>
-//       <div className={cls.imageBox}></div>
-//       <div className={cls.textContent}>
-//         <div className={cls.titleBar}></div>
-//         <div className={cls.subtitleBar}></div>
-//         <div className={cls.descBar}></div>
-//       </div>
-//     </div>
-//   </div>
-// );
-
-// const PlacesListSkeleton = () => (
-//   <div className={cls.placesListSkeleton}>
-//     <div className={cls.headerBar}></div>
-//     {[1, 2, 3, 4, 5].map((i) => (
-//       <PlaceCardSkeleton key={i} />
-//     ))}
-//   </div>
-// );
-
 export const MapSkeleton = () => (
   <div className={cls.mainPageSkeleton}>
     <div className={cls.mapContainer}>
       <Map />
-
-      {/* <div className={cls.floatingList}>
-        <PlacesListSkeleton />
-      </div> */}
-
-      <div className={cls.favoritesButton}>
-        <div className={cls.buttonSkeleton}></div>
-      </div>
     </div>
   </div>
 );
