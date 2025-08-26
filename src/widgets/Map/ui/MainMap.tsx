07d74337a40caba9cbdb57f942b7017cbde88de1
mikhailyatsenko <misha.ggg@gmail.com>
2025-08-26 16:10:35 +0200

    feat: font optimization, fix: map display condition

--- src/widgets/Map/ui/MainMap.tsx
+++ src/widgets/Map/ui/MainMap.tsx
@@ -1,10 +1,10 @@
 import { type MainMapProps } from 'widgets/Map/types';
 import { LoadMap } from 'features/LoadMap';
 
-export const MainMap = ({ placesGeo }: MainMapProps) => {
+export const MainMap = ({ placesGeo, moreDataLoading }: MainMapProps) => {
   return (
     <div style={{ width: '100dvw', height: 'calc(100dvh - 60px)', overflow: 'hidden' }}>
-      <LoadMap placesGeo={placesGeo} />
+      <LoadMap moreDataLoading={moreDataLoading} placesGeo={placesGeo} />
     </div>
   );
 };
