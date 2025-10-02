3364ae2da4673baef9d73317caeb5aa3b6314329
mikhailyatsenko <misha.ggg@gmail.com>
2025-10-02 12:29:18 +0200

    fix: modal content style

--- src/features/AccountSettings/components/PendingEmailInfoModal/ui/PendingEmailInfoModal.tsx
+++ src/features/AccountSettings/components/PendingEmailInfoModal/ui/PendingEmailInfoModal.tsx
@@ -15,8 +15,8 @@ export const PendingEmailInfoModal = ({ pendingEmail, onClose }: PendingEmailInf
         <div className={cls.container}>
           <h3 className={cls.title}>Please check your email</h3>
           <p className={cls.text}>
-            We&apos;ve sent a confirmation link to {pendingEmail ?? 'your email address'}. Please click the link to
-            confirm your new email.
+            We&apos;ve sent a confirmation link to <b>{pendingEmail ?? 'your email address'}</b>. Please click the link
+            to confirm your new email.
           </p>
           <p className={cls.text}>If you don&apos;t see the email, check your spam folder.</p>
           <div className={cls.actions}>
