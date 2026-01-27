b531494165db430e8aa94f6817a3fec4b5660e32
mikhailyatsenko <misha.ggg@gmail.com>
2026-01-27 10:21:59 +0100

    hotfix: badge new props

--- src/shared/ui/BadgePill/types/index.ts
+++ src/shared/ui/BadgePill/types/index.ts
@@ -1,9 +1,11 @@
 export type BadgePillColor = 'red' | 'green' | 'blue' | 'orange' | 'gray' | 'purple';
 export type BadgePillSize = 'small' | 'medium' | 'large';
+export type BadgePillHoverColor = 'red' | 'green' | 'blue' | 'orange' | 'gray' | 'purple';
 
 export interface BadgePillProps {
   text: string;
   color: BadgePillColor;
   size: BadgePillSize;
+  hover?: BadgePillHoverColor;
   className?: string;
 }
--- src/shared/ui/BadgePill/ui/BadgePill.tsx
+++ src/shared/ui/BadgePill/ui/BadgePill.tsx
@@ -2,6 +2,7 @@ import clsx from 'clsx';
 import { type BadgePillProps } from '../types';
 import cls from './BadgePill.module.scss';
 
-export const BadgePill = ({ text, color = 'green', size = 'small', className }: BadgePillProps) => {
-  return <div className={clsx(cls.badgePill, cls[size], cls[color], className)}>{text}</div>;
+export const BadgePill = ({ text, color = 'green', size = 'small', hover, className }: BadgePillProps) => {
+  const hoverClass = hover ? cls[`hover-${hover}`] : undefined;
+  return <div className={clsx(cls.badgePill, cls[size], cls[color], hoverClass, className)}>{text}</div>;
 };
